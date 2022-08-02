3d10e1302c0d721be570e1a9176fa024e0e14b71
dyleee <oyinxdoubx@gmail.com>
2022-08-02 18:15:31 +0200

    hm

--- main.go
+++ main.go
@@ -22,6 +22,21 @@ import (
 //go:embed config.yaml
 var configBytes []byte
 
+func init() {
+	lvl, ok := os.LookupEnv("LOG_LEVEL")
+	// LOG_LEVEL not set, let's default to info
+	if !ok {
+		lvl = "info"
+	}
+	// parse string, this is built-in feature of logrus
+	ll, err := log.ParseLevel(lvl)
+	if err != nil {
+		ll = log.InfoLevel
+	}
+	// set global log level
+	log.SetLevel(ll)
+}
+
 func main() {
 	// Load the config file
 	var config config.CoreConfig
